8899f5b95595f51f360f7ae97e9e84c6b91e70b6
PrinnoTech198 <innocent.pachalio@gmail.com>
2026-06-17 07:59:38 +0300

    add user

--- apps/users/password_utils.py
+++ apps/users/password_utils.py
@@ -6,13 +6,19 @@ from apps.users.models import User
 def apply_user_password(user: User, password: str, *, record_for_admin: bool = True) -> None:
     """Hash password and optionally store a copy for super-admin credential export."""
     user.set_password(password)
-    update_fields = ["password"]
     if record_for_admin:
         user.admin_password = password
-        update_fields.append("admin_password")
-    user.save(update_fields=update_fields)
+    if user.pk:
+        update_fields = ["password"]
+        if record_for_admin:
+            update_fields.append("admin_password")
+        user.save(update_fields=update_fields)
+        return
+    user.save()
 
 
 def clear_admin_password_record(user: User) -> None:
     """Remove stored admin password after the user changes it themselves."""
     user.admin_password = ""
+    if user.pk:
+        user.save(update_fields=["admin_password"])
